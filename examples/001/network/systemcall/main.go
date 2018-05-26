17240390d5f0f0e72a620423db5724d7e845c31d
mushroomsir <mushroomsir@163.com>
2018-05-26 14:25:21 +0800

    feat: add new blog

---
+++ examples/001/network/systemcall/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mushroomsir/blog/examples/util"
+	"golang.org/x/net/ipv4"
+	"os"
+	"syscall"
+)
+
+func main() {
+	fd, _ := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, 1)
+	f := os.NewFile(uintptr(fd), fmt.Sprintf("fd %d", fd))
+	for {
+		buf := make([]byte, 1500)
+		f.Read(buf)
+		ip4header, _ := ipv4.ParseHeader(buf[:20])
+		fmt.Println("ipheader:", ip4header)
+		tcpheader := util.NewTCPHeader(buf[20:40])
+		fmt.Println("tcpheader:", tcpheader)
+	}
+}
